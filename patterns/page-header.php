5454c666e3d939c2518901542e74e39596b1a54f
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-08-12 17:52:48 +0200

    Update page header to lock and use featured image

--- patterns/page-header.php
+++ patterns/page-header.php
@@ -6,11 +6,10 @@
  * Block Types: page
  */
 
-$image_url = get_template_directory_uri() . '/assets/img/default-image.jpg';
 ?>
 
-<!-- wp:cover {"url":"<?php echo $image_url; ?>","dimRatio":90,"focalPoint":{"x":"0.50","y":"0.50"},"gradient":"wordpress-blue","contentPosition":"bottom left","align":"full"} -->
-<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-left"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim wp-block-cover__gradient-background has-background-gradient has-wordpress-blue-gradient-background"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo $image_url; ?>" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%" />
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":90,"focalPoint":{"x":"0.50","y":"0.50"},"minHeight":20,"minHeightUnit":"vw","gradient":"wordpress-blue","contentPosition":"bottom left","isDark":false,"lock":{"move":true,"remove":true},"align":"full"} -->
+<div class="wp-block-cover alignfull is-light has-custom-content-position is-position-bottom-left" style="min-height:20vw"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim has-background-gradient has-wordpress-blue-gradient-background"></span>
 	<div class="wp-block-cover__inner-container">
 		<!-- wp:group {"layout":{"inherit":true}} -->
 		<div class="wp-block-group">
