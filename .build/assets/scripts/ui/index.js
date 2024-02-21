264e976b89243d78735b9b04ec57ed82be57c612
Mark Howells-Mead <permanent.tourist@gmail.com>
2024-02-21 17:19:09 +0100

    Revise SVH logic

--- .build/assets/scripts/ui/index.js
+++ .build/assets/scripts/ui/index.js
@@ -5,18 +5,5 @@ import './aria-toggler';
 import './header';
 
 console.log('%cDeveloped by', 'font-style: italic; font-size: 12px;');
-console.log(
-	'%cSay Hello GmbH',
-	'font-weight: bold; color: #000; font-size: 16px;'
-);
-console.log('%chttps://sayhello.ch', 'color: #000; font-size: 12px;');
-
-// Load script if svh is not supported natively
-if (!CSS.supports || !CSS.supports('height', '1svh')) {
-	let script = document.createElement('script');
-	script.setAttribute(
-		'src',
-		`${sht_theme.directory_uri}/assets/scripts/svh.min.js?version=${sht_theme.version}`
-	);
-	document.head.appendChild(script);
-}
+console.log('%cMark Howells-Mead', 'font-weight: bold; color: #000; font-size: 16px;');
+console.log('%chttps://permanenttourist.ch', 'color: #000; font-size: 12px;');
