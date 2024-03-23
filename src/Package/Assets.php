331f14e93caf339357dad39c73db5d9fc69f121e
Mark Howells-Mead <mark@sayhello.ch>
2024-03-23 15:41:13 +0100

    Simplify debug and min logic

--- src/Package/Assets.php
+++ src/Package/Assets.php
@@ -12,6 +12,12 @@ class Assets
 
 	public $theme_url = '';
 	public $theme_path = '';
+	private $debug = false;
+
+	public function __construct()
+	{
+		$this->debug = defined('WP_DEBUG') && WP_DEBUG === false;
+	}
 
 	public function run()
 	{
@@ -28,8 +34,6 @@ class Assets
 			wp_deregister_style('dashicons');
 		}
 
-		$min = !sht_theme()->debug;
-
 		/**
 		 * CSS
 		 */
@@ -38,7 +42,7 @@ class Assets
 		wp_enqueue_style('fancybox', get_template_directory_uri() . '/assets/plugins/fancybox/jquery.fancybox.min.css', [], '3.4.0');
 		$deps[] = 'fancybox';
 
-		wp_enqueue_style('sht-style', get_template_directory_uri() . '/assets/styles/ui' . ($min ? '.min' : '') . '.css', $deps, filemtime(get_template_directory() . '/assets/styles/ui' . ($min ? '.min' : '') . '.css'));
+		wp_enqueue_style('sht-style', get_template_directory_uri() . '/assets/styles/ui' . (!$this->debug ? '.min' : '') . '.css', $deps, filemtime(get_template_directory() . '/assets/styles/ui' . (!$this->debug ? '.min' : '') . '.css'));
 
 		// Javascript
 		$deps = [];
@@ -46,7 +50,7 @@ class Assets
 		wp_enqueue_script('fancybox', get_template_directory_uri() . '/assets/plugins/fancybox/jquery.fancybox.min.js', ['jquery'], '3.4.0', true);
 		$deps[] = 'fancybox';
 
-		wp_enqueue_script('sht-script', get_template_directory_uri() . '/assets/scripts/ui' . ($min ? '.min' : '') . '.js', $deps, filemtime(get_template_directory() . '/assets/scripts/ui' . ($min ? '.min' : '') . '.js'), true);
+		wp_enqueue_script('sht-script', get_template_directory_uri() . '/assets/scripts/ui' . (!$this->debug ? '.min' : '') . '.js', $deps, filemtime(get_template_directory() . '/assets/scripts/ui' . (!$this->debug ? '.min' : '') . '.js'), true);
 		wp_localize_script('sht-script', 'sht_theme', [
 			'directory_uri' => get_template_directory_uri(),
 			'version' => wp_get_theme()->get('Version')
@@ -55,17 +59,13 @@ class Assets
 
 	public function registerAdminAssets()
 	{
-		//wp_enqueue_style('sht-admin-editor-style', get_template_directory_uri() . '/assets/styles/admin-editor' . (sht_theme()->debug ? '' : '.min') . '.css', ['wp-edit-blocks'], filemtime(get_template_directory() . '/assets/styles/admin-editor' . (sht_theme()->debug ? '' : '.min') . '.css'));
-		wp_enqueue_style('sht-admin-style', get_template_directory_uri() . '/assets/styles/admin' . (sht_theme()->debug ? '' : '.min') . '.css', ['sht-admin-editor-style', 'wp-edit-blocks'], filemtime(get_template_directory() . '/assets/styles/admin' . (sht_theme()->debug ? '' : '.min') . '.css'));
+		wp_enqueue_style('sht-admin-style', get_template_directory_uri() . '/assets/styles/admin' . ($this->debug ? '' : '.min') . '.css', ['sht-admin-editor-style', 'wp-edit-blocks'], filemtime(get_template_directory() . '/assets/styles/admin' . ($this->debug ? '' : '.min') . '.css'));
 	}
 
 	public function editorStyle()
 	{
 		add_theme_support('editor-styles');
-		add_editor_style('assets/styles/admin-editor' . (sht_theme()->debug ? '' : '.min') . '.css');
-		// if (file_exists(get_template_directory() . '/assets/styles/admin-editor' . (sht_theme()->debug ? '' : '.min') . '.css')) {
-		// 	add_editor_style('assets/styles/admin-editor' . (sht_theme()->debug ? '' : '.min') . '.css');
-		// }
+		add_editor_style('assets/styles/admin-editor' . ($this->debug ? '' : '.min') . '.css');
 	}
 
 	public function loadFonts()
