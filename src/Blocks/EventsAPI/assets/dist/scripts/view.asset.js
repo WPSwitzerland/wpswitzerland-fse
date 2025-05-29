772f0728ae474037cf3bf752afbef73582370dab
Mark Howells-Mead <permanent.tourist@gmail.com>
2025-05-29 16:04:10 +0200

    NPM updates

--- src/Blocks/EventsAPI/assets/dist/scripts/view.asset.js
+++ src/Blocks/EventsAPI/assets/dist/scripts/view.asset.js
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '45c714a4d78c24191bd0');
+<?php return array('dependencies' => array('wp-element'), 'version' => '4a0f07ce24edd73081a7');
